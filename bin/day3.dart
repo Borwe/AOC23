761795bcdcb733a93b554cee5a2b2fe0a015d986
brian.orwe <brian.orwe@gmail.com>
2023-12-04 21:34:43 +0300

    Done day 3

--- bin/day3.dart
+++ bin/day3.dart
@@ -48,11 +48,6 @@ class Nums {
     final width = right - left;
     final n = int.parse(lines[y].substring(left, right));
 
-    //print("NUM: $n");
-    if (n == 449) {
-      print("${lines[y]}");
-    }
-
     return Nums(n, p, width);
   }
 
@@ -71,7 +66,6 @@ int part1(List<String> lines) {
   }
 
   for (final p in points) {
-    print("${p.mX}, ${p.mY}");
     List<Nums?> numsSearch = [];
     //north
     if (p.mY - 1 >= 0) {
@@ -135,8 +129,100 @@ int part1(List<String> lines) {
   return sum;
 }
 
+int part2(List<String> lines) {
+  List<int> nums = [];
+  List<Point> points = [];
+  for (var y = 0; y < lines.length; ++y) {
+    for (var x = 0; x < lines[y].length; ++x) {
+      if (lines[y][x] != '.' && int.tryParse(lines[y][x]) == null) {
+        if (lines[y][x] == "*") {
+          points.add(Point(x, y));
+        }
+      }
+    }
+  }
+
+  for (final p in points) {
+    List<Nums?> numsSearch = [];
+    //north
+    if (p.mY - 1 >= 0) {
+      numsSearch.add(Nums.from(lines, p.mX, p.mY - 1));
+    }
+
+    //neast
+    if (p.mY - 1 >= 0 && p.mX + 1 < lines[0].length) {
+      numsSearch.add(Nums.from(lines, p.mX + 1, p.mY - 1));
+    }
+
+    //east
+    if (p.mX + 1 < lines[0].length) {
+      numsSearch.add(Nums.from(lines, p.mX + 1, p.mY));
+    }
+
+    //seast
+    if (p.mY + 1 < lines.length && p.mX + 1 < lines[0].length) {
+      numsSearch.add(Nums.from(lines, p.mX + 1, p.mY + 1));
+    }
+
+    //south
+    if (p.mY + 1 < lines.length) {
+      numsSearch.add(Nums.from(lines, p.mX, p.mY + 1));
+    }
+
+    //swest
+    if (p.mY + 1 < lines.length && p.mX - 1 > 0) {
+      numsSearch.add(Nums.from(lines, p.mX - 1, p.mY + 1));
+    }
+
+    //west
+    if (p.mX - 1 >= 0) {
+      numsSearch.add(Nums.from(lines, p.mX - 1, p.mY));
+    }
+
+    //nwest
+    if (p.mY - 1 >= 0 && p.mY - 1 >= 0) {
+      numsSearch.add(Nums.from(lines, p.mX - 1, p.mY - 1));
+    }
+
+    //stop repeating
+    List<Nums> used = [];
+    var multiple = 1;
+    var counts = 0;
+    for (final num in numsSearch) {
+      if (counts > 2) {
+        break;
+      }
+      if (num != null) {
+        var match = false;
+        for (final n in used) {
+          if (n.isSameAtPoint(num)) {
+            match = true;
+          }
+        }
+        if (match == false) {
+          counts += 1;
+          multiple = num.num * multiple;
+          used.add(num);
+        }
+      }
+    }
+
+    if(counts==2){
+        nums.add(multiple);
+    }
+  }
+
+  var sum = 0;
+  for (final n in nums) {
+    sum += n;
+  }
+  return sum;
+}
+
 void main() {
+  print("Day 3");
   final lines = File("./data/day3.txt").readAsLinesSync();
 
   print("Part1: ${part1(lines)}");
+  print("Part2: ${part2(lines)}");
 }
